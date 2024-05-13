d42a461e005ae308ce4d0f18438e825d92ed4fd9
LamaraRurua <142101778+LamaraRurua@users.noreply.github.com>
2024-05-13 20:22:34 +0400

    Add files via upload

---
+++ arrays.php
@@ -0,0 +1,38 @@
+<?php
+$cards= ( 
+
+    [ 'title' => 'Lingerie', 
+    'image' => 'img/div 1.png',
+    'button' => ' <a href="#" class="button"> View More </button>', ],
+ 
+    
+
+    [ 'title' => 'Spring ', 
+    'image' => 'img/div 2.png',
+    'button' => ' <a href="#" class="button"> View More </button>', ],
+
+    [ 'title' => 'Yoga ', 
+    'image' => 'img/div 3.png' ,
+    'button' => ' <a href="#" class="button"> View More </button>', ],
+
+
+    [ 'title' => 'Glove ', 
+    'image' => 'img/div 4.png' ,
+    'button' => ' <a href="#" class="button"> View More </button>', ],
+
+    [ 'title' => 'Ensembles ', 
+    'image' => 'img/div 5.png' ,
+    'button' => ' <a href="#" class="button"> View More </button>', ],
+
+
+    [ 'title' => 'Skin Care ', 
+    'image' => 'img/div 5.png' ,
+    'button' => ' <a href="#" class="button"> View More </button>', ],
+
+);
+?>
+
+
+
+
+
